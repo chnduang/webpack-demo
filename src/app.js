77f8968c8b4faf1f2b3ce882e5c273eef6d7423a
zhouqd <zhouqd1997@163.com>
2021-09-08 11:15:31 +0800

    [ADD] 动态导入组件

--- src/app.js
+++ src/app.js
@@ -8,40 +8,42 @@ import DemoSvgImg from "./assets/work-order.svg";
 // import json from "./assets/data/data.json5";
 import demoFunc from "./demo.js";
 
-const renderDom = () => {
-  const ele = document.createElement("div");
-  const btn = document.createElement("button");
-  ele.innerHTML = join(["webpack", "demo"], "-");
-  const classnameList = ["container"];
-  ele.classList.add(...classnameList);
-
-  const eleImg = new Image();
-  eleImg.src = DemoSvgImg;
-  eleImg.className = "img";
-  ele.appendChild(eleImg);
-
-  btn.innerHTML = "click me332";
-  btn.onclick = demoFunc;
-  ele.appendChild(btn);
-
-  //   console.table("dataCsv", dataCsv);
-  //   console.log("dataXml", dataXml);
-
-  //   console.log(toml.title); // output `TOML Example`
-  //   console.log(yaml.title); // output `YAML Example`
-  //   console.log(json.title); // output `JSON5 Example`
-
-  return ele;
-};
-
-// const renderDom = async () => {
+// const renderDom = () => {
 //   const ele = document.createElement("div");
-//   const { default: _ } = await import("lodash");
+//   const btn = document.createElement("button");
 //   ele.innerHTML = join(["webpack", "demo"], "-");
+//   const classnameList = ["container"];
+//   ele.classList.add(...classnameList);
+
+//   const eleImg = new Image();
+//   eleImg.src = DemoSvgImg;
+//   eleImg.className = "img";
+//   ele.appendChild(eleImg);
+
+//   btn.innerHTML = "click me332";
+//   btn.onclick = demoFunc;
+//   ele.appendChild(btn);
+
+//   //   console.table("dataCsv", dataCsv);
+//   //   console.log("dataXml", dataXml);
+
+//   //   console.log(toml.title); // output `TOML Example`
+//   //   console.log(yaml.title); // output `YAML Example`
+//   //   console.log(json.title); // output `JSON5 Example`
 
 //   return ele;
 // };
 
-// renderDom.then(dom => {document.body.appendChild(dom)})
+const renderDom = async () => {
+  const ele = document.createElement("div");
+  const { default: _ } = await import("lodash");
+  ele.innerHTML = join(["webpack", "demo"], "-");
+
+  return ele;
+};
+
+renderDom.then((dom) => {
+  document.body.appendChild(dom);
+});
 
 document.body.appendChild(renderDom());
